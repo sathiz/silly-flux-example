d6ab082b242904b229f9fd34e7279cd37bb642d2
Max Nachlinger <max.nachlinger@gmail.com>
2014-07-26 17:45:35 -0700

    Build / deploy work

--- gulpfile.js
+++ gulpfile.js
@@ -4,65 +4,73 @@ var gutil = require('gulp-util');
 var browserify = require('browserify');
 var reactify = require('reactify');
 var watchify = require('watchify');
-var notify = require("gulp-notify");
+var notify = require('gulp-notify');
 var uglify = require('gulp-uglify');
 var streamify = require('gulp-streamify');
-
-var scriptsDir = './src/js';
-var buildDir = './dist/js';
+var minifyCss = require('gulp-minify-css');
+var usemin = require('gulp-usemin');
+var minifyHtml = require('gulp-minify-html');
+var rev = require('gulp-rev');
 
 function handleErrors() {
 	var args = Array.prototype.slice.call(arguments);
 	notify.onError({
-		title: "Error",
-		message: "<%= error.message %>"
+		title: 'Error',
+		message: '<%= error.message %>'
 	}).apply(this, args);
 	this.emit('end'); // Keep gulp from hanging on this task
 }
 
 // Based on: http://blog.avisi.nl/2014/04/25/how-to-keep-a-fast-build-with-browserify-and-reactjs/
+function buildJs(params) {
+	var destJsFile = params.file;
+	var deploy = params.deploy;
 
-function buildScript(params) {
-	var file = params.file;
-	var watch = params.watch;
-
-	return function() {
-		var props = {entries: [scriptsDir + '/' + file]};
-		var bundler = watch ? watchify(props) : browserify(props);
+	return function () {
+		var entries = {entries: ['./src/js/' + destJsFile]};
+		var bundler = deploy ? browserify(entries) : watchify(entries);
 
 		bundler.transform(reactify);
 
 		function rebundle() {
-			var stream = bundler.bundle({debug: watch})
+			var stream = bundler.bundle({debug: !deploy})
 				.on('error', handleErrors)
-				.pipe(source(file));
+				.pipe(source(destJsFile));
 
-			if(!watch)
-				stream.pipe(streamify(uglify()));
-
-			return stream.pipe(gulp.dest(buildDir + '/'));
+			if (deploy) {
+				return stream
+					.pipe(streamify(uglify()))
+					.pipe(streamify(rev()));
+			}
+			return stream.pipe(gulp.dest('dist/js/'));
 		}
 
 		bundler.on('update', function () {
 			rebundle();
-			gutil.log('Rebundle...');
+			gutil.log('Rebundle');
 		});
 
 		return rebundle();
 	};
 }
 
-gulp.task('copy', function () {
-	gulp.src('src/**/*.html', {base: './src'})
-		.pipe(gulp.dest('./dist/'));
-	gulp.src('src/css/**/*', {base: './src/css'})
-		.pipe(gulp.dest('./dist/css/'));
-	gulp.src('src/images/**/*', {base: './src/images'})
+gulp.task('copy-images', function () {
+	gulp.src('./src/images/**/*', {base: './src/images'})
+		.on('error', handleErrors)
 		.pipe(gulp.dest('./dist/images/'));
 });
 
-gulp.task('build', buildScript({file: 'main.js', watch: false}));
-gulp.task('watch', buildScript({file: 'main.js', watch: true}));
+gulp.task('usemin', function () {
+	gulp.src('./src/index.html')
+		.pipe(usemin({
+			css: [minifyCss(), 'concat'],
+			html: [minifyHtml({empty: true})]
+		}))
+		.pipe(gulp.dest('dist/'));
+});
 
-gulp.task('default', ['copy', 'watch']);
-gulp.task('deploy', ['copy', 'build']);
+gulp.task('build', buildJs({file: 'main.js', deploy: true}));
+gulp.task('watch', buildJs({file: 'main.js', deploy: false}));
+
+gulp.task('default', ['usemin', 'copy-images', 'watch']);
+gulp.task('deploy', ['usemin', 'copy-images', 'build']);
