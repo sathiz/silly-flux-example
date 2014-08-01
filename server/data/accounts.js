78acdcc77e86fb625b757974f9ecf33015ceebdd
Max Nachlinger <max.nachlinger@gmail.com>
2014-08-01 09:36:22 -0700

    Added more awesome account-names

--- server/data/accounts.js
+++ server/data/accounts.js
@@ -21,8 +21,8 @@ function getRandomValue(arr, len) {
 }
 
 var accounts = _.map(_.range(0, 100), function () {
-	var adjective = getRandomValue(adjectives, adjectivesLength);
-	var domainName = adjective.toLowerCase() + '.com';
+	var adjective = getRandomValue(adjectives, adjectivesLength) + ' ' + getRandomValue(adjectives, adjectivesLength);
+	var domainName = adjective.toLowerCase().replace(/\s+/, '-') + '.com';
 
 	var amtUsers = getRandomInt(1, 15);
 	var users = _.map(_.range(0, amtUsers), makeAdmin(domainName));
